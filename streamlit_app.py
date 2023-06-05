1f97c99e67fcc16e75a738ea6a9c45a995786515
Revoman121 <135415064+Revoman121@users.noreply.github.com>
2023-06-05 13:14:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ from PIL import Image
 import io
 
 # Set up the Tesseract OCR executable path
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract
+'
 
 
 # Define a function to perform OCR on the selected image and display the result
